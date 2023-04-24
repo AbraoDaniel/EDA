961b40128d831bb3a0962a5dbdb929dcddfcccbd
Thuany <50593898+Thurunany@users.noreply.github.com>
2023-04-24 21:33:24 +0000

    Inserindo matriz

--- TRABALHO_2/Principal.c
+++ TRABALHO_2/Principal.c
@@ -2,3 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Pilha.h"
+
+int main(){
+    // Entrada de dados
+    int n;
+    printf("Qual será o tamanho de sua matriz?");
+    scanf("%d", &n);
+    int m[n][n];
+
+    printf("Entre com os valores da matriz:\n");
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            printf("Matriz[%d][%d]: ", i, j);
+            scanf("%d", &m[i][j]);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
