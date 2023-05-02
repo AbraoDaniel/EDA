02903ee1af7ec2a62c728f21cb53a500b0c255ca
Thuany <50593898+Thurunany@users.noreply.github.com>
2023-05-02 02:45:08 +0000

    enunciado

--- LISTA4 FILAS/ativ_01.c
+++ LISTA4 FILAS/ativ_01.c
@@ -1,3 +1,13 @@
+/*
+gcc ./estatica/Fila.c Pilha.c ativ_01.c  && ./a.out 
+
+Escreva um programa que insere alguns valores em uma fila. Em seguida, ele deve
+inverter a ordem de seus valores utilizando uma pilha. Finalmente, os valores da fila
+devem ser mostrados. A implementação deve ser feita utilizando-se apenas as operações
+dos TAD Fila e Pilha (inserir, remover, pilha_vazia, empilha, etc.), não acesse
+diretamente os elementos da struct
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "dinamica/Fila.h"
