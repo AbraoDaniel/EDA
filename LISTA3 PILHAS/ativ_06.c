680df64fa805ba1269483fc17d0bf848a29f55e2
Thuany <50593898+Thurunany@users.noreply.github.com>
2023-05-02 00:49:37 +0000

    Ativ 06

---
+++ LISTA3 PILHAS/ativ_06.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Pilha.h"
+
+Pilha p;
+
+int main(int argc, char *argv[]) {
+    char exp[] = "((2+4)*6)/3";
+    int tam= strlen(exp);
+    int i, lixo;
+    inicializa_pilha(&p, tam);
+
+    for(i=0; exp[i] != 0; i++){
+        if(exp[i]=='('){
+            empilha(&p, exp[i]);
+        } else
+        if(exp[i]==')'){
+            if(pilha_vazia(p)){
+                printf("Fecha sem abre\n");
+                return 1;
+            }else{
+                desempilha(&p, &lixo);
+            }
+        }
+    }
+    if(!pilha_vazia(p)){
+            printf("Parenteses aberto e não fechado\n");
+        }else{
+             printf("Expressão ok!\n");
+        }
+    desaloca_pilha(&p);
+
+    return 1;
+}
