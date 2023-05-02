2eb8a49d157389dda793de8c52749e65aa6140eb
Thuany <50593898+Thurunany@users.noreply.github.com>
2023-05-02 18:28:04 +0000

    Ativ 02

---
+++ LISTA4 FILAS/ativ2/ativ_02.c
@@ -0,0 +1,48 @@
+/*
+Implemente uma nova biblioteca de Filas, contendo os seguintes campos: dados (ponteiro
+int, capacidade e fim. Esta fila não é circular, sendo que o fim é variável (índice), mas o
+início é fixo na posição 0 do vetor. Portanto, é preciso mover os dados para a esquerda a
+cada remoção da fila
+
+FIM = ( inicio + n ) % MAX
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "Fila.h"
+
+Fila f;
+
+
+int main(){
+    printf("Qual o tamanho da lista? ");
+    int x, aux;
+    scanf("%d", &x);
+    char fila[x];
+    inicializa_fila(&f, x);
+
+    for(int i=0; i < x; i++){
+        scanf("%d", &aux);
+        inserir(&f, aux);
+    }
+    mostra_fila(f);
+
+    printf("REMOVENDO VALOR DA FILA!\n\n");
+    remover(&f, &aux);
+
+
+    for(int i= 0; !fila_vazia(f); i++){
+        remover(&f, &aux);
+        fila[i] = aux;
+    }
+
+    for(int i=0; fila[i] != 0; i++){
+        inserir(&f, fila[i]);
+    }
+    mostra_fila(f);
+
+
+
+    return 1;
+}
