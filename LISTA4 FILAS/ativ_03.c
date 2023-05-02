d00d436541fab985ee368a1de07d1d9dd35fd619
Thuany <50593898+Thurunany@users.noreply.github.com>
2023-05-02 11:51:17 +0000

    enunciado

--- LISTA4 FILAS/ativ_03.c
+++ LISTA4 FILAS/ativ_03.c
@@ -1,3 +1,11 @@
+/*
+Implemente mais uma biblioteca de Filas, desta vez usando duas pilhas. Faça o include
+da biblioteca de pilhas e declare a struct da fila com duas pilhas (p1 e p2) somente. A
+implementação das funções da fila deve ser feita utilizando-se apenas as funções de
+manipulação de pilhas (empilha, desempilha, pilha_cheia, etc.). Portanto, não é permitido
+acessar diretamente os elementos da struct Pilha.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "Pilha.h"
