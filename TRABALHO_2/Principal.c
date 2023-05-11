df7403e25efe9b3343d31c5d845a17dbb3e7387a
Thuany <50593898+Thurunany@users.noreply.github.com>
2023-05-10 22:45:36 -0300

    TRABALHO FINAL

--- TRABALHO_2/Principal.c
+++ TRABALHO_2/Principal.c
@@ -1,33 +1,115 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
+#include <stdio.h>
+#include "Fila.h"
+#include "PIlha.h"
 
-int main(){
-    // Entrada de dados
-    int n, e, s;
-    printf("Qual será o tamanho de sua matriz?");
-    scanf("%d", &n);
-    int m[n][n];
+Fila f;
+Pilha p;
 
-    printf("Entre com os valores da matriz:\n");
+int main()
+{
+  FILE *arquivo;
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            printf("Matriz[%d][%d]: ", i, j);
-            scanf("%d", &m[i][j]);
-        }
+  arquivo = fopen("arquivo.txt", "r");
+  if (arquivo == NULL)
+  {
+    printf("Erro ao abrir o arquivo");
+    return 1;
+  }
+
+  // DEFININDO TAMANHO DA MATRIZ E ALOCANDO OS VALORES
+
+  int n;
+  printf("Defina o tamanho da matriz desejada: ");
+  scanf("%d", &n);
+
+  int matriz[n + 1][n + 1];
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n; j++)
+    {
+      fscanf(arquivo, "%d", &matriz[i][j]);
     }
+  }
 
-    printf("Qual o ponto inicial do percurso? ");
-    scanf("%d", &e);
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n; j++)
+    {
+      int valor;
+      printf("%4d", matriz[i][j]);
+    }
+    printf("\n");
+  }
 
-    printf("Onde quer chegar? ");
-    scanf("%d\n", &s);
+  // INFORMANDO VERTICE INICIAL E FINAL
 
-    bool vs[n];
-    bool va[n];
+  int vertice_inicial, vertice_final;
+  printf("Informe o vértice inicial: ");
+  scanf("%d", &vertice_inicial);
+
+  printf("Informe o vértice final: ");
+  scanf("%d", &vertice_final);
+
+  // INICIALIZA VETOR DE STATUS (VS) e VETOR DE ANTECESSORES (VA)
+
+  int vs[n];
+  int va[n];
+  for (int i = 0; i < n; i++)
+  {
+    vs[i] = 0;
+    va[i] = 0;
+  }
+
+  // INICIALIZANDO FILA
+  inicializa_fila(&f, n);
+
+  // DEFININDO V[A] COMO DEFINIDO
+  // inicia BFS
+  vs[vertice_inicial] = 1;
+  inserir(&f, vertice_inicial);
+
+  int achou = 0, aux = 0;
+  while (!fila_vazia(f) && !achou){
+
+    // estou analizando aux
+    remover(&f, &aux);
+    if (aux == vertice_final){
+      achou = 1;
+    }
+ 
+ 
+    else{
+      for (int j = 1; j <= n; j++){
+        if (matriz[aux][j] == 1){
+          if (vs[j] == 0){
+            vs[j] = 1;
+            va[j] = aux;
+            inserir(&f, j);
+          }
+        }
+      }
+    }
+  }
 
 
-    return EXIT_SUCCESS;
+
+  if (achou){
+    inicializa_pilha(&p, n);
+    while (aux != 0){
+      empilha(&p, aux);
+      aux = va[aux];
+    }
+    while (!pilha_vazia(p)){
+      desempilha(&p, &aux);
+      printf("%d \n", aux);
+    }
+  }
+  else
+  {
+    printf("B não é alcançável a partir de A!");
+  }
+
+  return 0;
 }
